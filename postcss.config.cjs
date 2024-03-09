cb64e8d8fc5bf1046a154cea61bbce4048c7db89
Karen Moreno <karenmorales.2918@gmail.com>
2024-03-09 16:26:49 -0400

    calculando prestamos

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Otros plugins de PostCSS que desees utilizar
+  ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // Agrega aquí las fuentes de contenido que deseas analizar
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
